63ca0fdafddf7ea8af235e664c44583367706e12
Md Faysal Ahamed <44522552+Faysal425@users.noreply.github.com>
2023-08-25 10:02:00 +0600

    model.py

--- model.py
+++ model.py
@@ -40,34 +40,30 @@ class InceptionResNetV2UNet:
         encoder = InceptionResNetV2(include_top=False, weights="imagenet", input_tensor=inputs)
 
         """ Encoder """
-        s1 = encoder.layers[0].output  ## (512 x 512)
+        s1 = encoder.layers[0].output 
 
-        s2 = encoder.layers[3].output  ## (255 x 255)
-        s2 = ZeroPadding2D(((1, 0), (1, 0)))(s2)  ## (256 x 256)
+        s2 = encoder.layers[3].output 
+        s2 = ZeroPadding2D(((1, 0), (1, 0)))(s2)  
 
-        s3 = encoder.layers[13].output  ## (126 x 126)
-        s3 = ZeroPadding2D((1, 1))(s3)  ## (128 x 128)
-
-        s4 = encoder.layers[265].output  ## (61 x 61)
-        s4 = ZeroPadding2D(((2, 1), (2, 1)))(s4)  ## (64 x 64)
+        s3 = encoder.layers[13].output  
+        s3 = ZeroPadding2D((1, 1))(s3) 
+        
+        s4 = encoder.layers[265].output  
+        s4 = ZeroPadding2D(((2, 1), (2, 1)))(s4) 
 
         """ Bridge """
-        b1 = encoder.layers[501].output  ## (30 x 30)
-        b1 = ZeroPadding2D((1, 1))(b1)  ## (32 x 32)
+        b1 = encoder.layers[501].output  
+        b1 = ZeroPadding2D((1, 1))(b1)  
 
         """ Decoder """
-        d1 = decoder_block(b1, s4, 512)  ## (64 x 64)
-        d2 = decoder_block(d1, s3, 256)  ## (128 x 128)
-        d3 = decoder_block(d2, s2, 128)  ## (256 x 256)
-        d4 = decoder_block(d3, s1, 64)  ## (512 x 512)
+        d1 = decoder_block(b1, s4, 512)  
+        d2 = decoder_block(d1, s3, 256)  
+        d3 = decoder_block(d2, s2, 128)  
+        d4 = decoder_block(d3, s1, 64)  
 
         """ Output """
         outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
         print(outputs)
         model = Model(inputs, outputs, name="InceptionResNetV2_UNet")
         return model
-# if __name__ == "__main__":
-#     # model = InceptionResNetV2UNet()
-#     # print(input_size)
-#     from torchsummary import summary
-#     summary(model, (256,256,3))
+
